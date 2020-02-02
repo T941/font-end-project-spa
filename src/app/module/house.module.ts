ae337d1dd9a54d2a77e0991f6474bfdcee0dd485
Sang <tranquocsang19977@gmail.com>
2020-02-02 19:30:22 +0700

    fix deploy

--- src/app/module/house.module.ts
+++ src/app/module/house.module.ts
@@ -6,7 +6,6 @@ import {DetailHouseComponent} from '../Components/Pages/house/detail-house/detai
 import {CreateHouseComponent} from '../Components/Pages/house/create-house/create-house.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {EditHouseComponent} from '../Components/Pages/house/edit-house/edit-house.component';
-import {HouseTableComponent} from '../Components/Pages/house/search/components/house/house-table/house-table.component';
 
 const routing: Routes = [
   {
@@ -30,8 +29,7 @@ const routing: Routes = [
   declarations: [
     DetailHouseComponent,
     CreateHouseComponent,
-    EditHouseComponent,
-    HouseTableComponent
+    EditHouseComponent
   ],
   imports: [
     CommonModule,
